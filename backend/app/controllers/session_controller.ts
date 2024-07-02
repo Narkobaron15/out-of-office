ce082b2be99241bb99f1f37f509c0a82d8df1ff5
Alex Ivanov <oleksii120@gmail.com>
2024-07-02 12:22:19 +0300

    Configured auth

---
+++ backend/app/controllers/session_controller.ts
@@ -0,0 +1,28 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import Employee from '#models/employee'
+
+export default class SessionController {
+  async login({ auth, request, response }: HttpContext) {
+    const { username, password } = request.all()
+    const user = await Employee.verifyCredentials(username, password)
+    await auth.use('web').login(user, true)
+    return response.noContent()
+  }
+
+  async logout({ auth, response }: HttpContext) {
+    await auth.use('web').logout()
+    return response.noContent()
+  }
+
+  async check({ auth, response }: HttpContext) {
+    const user = await auth.use('web').authenticate()
+    return response.json(user)
+  }
+
+  async register({ auth, request, response }: HttpContext) {
+    const newbie = request.all()
+    const user = await Employee.create(newbie)
+    await auth.use('web').login(user, true)
+    return response.noContent()
+  }
+}
--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -8,18 +8,34 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
 
+// API Routes
 router
   .group(() => {
-    router.get('/', async () => {
-      return { hello: 'world' }
-    })
+    // unauthenticated only routes group
+    router
+      .group(() => {
+        router.post('login', 'SessionController.login')
+        router.post('register', 'SessionController.register')
+        router.post('logout', 'SessionController.logout')
+      })
+      .use(middleware.guest())
+
+    // authenticated only routes group
+    router
+      .group(() => {
+        router.get('check', 'SessionController.check')
+
+
+      })
+      .use(middleware.auth())
   })
   .prefix('api/v1')
 
 // 404
 router.any('*', async ({ response }) => {
   return response.notFound({
-    error: '404 Not Found',
+    error: '404 Not Found'
   })
 })
